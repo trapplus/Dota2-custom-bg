44ee45ba14efed3909b152ebd6846c81bb971a0a
trapplus <blazer19092008@gmail.com>
2026-01-06 16:32:00 +0300

    Create linux shell script, update .bat(wo validation), update complited bg file index to 03, update README.md

---
+++ vkp-create-tool/create-vpk-linux.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+
+VPK_DIR="pak03_dir"
+VIDEO_DIR="$VPK_DIR/123"
+VIDEO_FILE="$VIDEO_DIR/123.webm"
+
+echo "=============================="
+echo " СБОРКА VPK ИЗ pak03_dir"
+echo " Внимание:"
+echo " - Используется vpk.exe - запуск через wine"
+echo " - Убедитесь, что wine установлен"
+echo "=============================="
+echo
+
+# Валидацияя бинаря
+if [[ -x "./vpk" ]]; then
+  VPK_CMD="./vpk"
+elif [[ -f "./vpk.exe" ]]; then
+  VPK_CMD="wine vpk.exe"
+else
+  echo "[ОШИБКА] vpk или vpk.exe не найден в текущей директории."
+  echo "Скрипт должен лежать рядом с vpk.exe"
+  read -r
+  exit 1
+fi
+
+# Валидация директории
+if [[ ! -d "$VIDEO_DIR" ]]; then
+  echo "[ОШИБКА] Папка $VIDEO_DIR не найдена."
+  read -r
+  exit 1
+fi
+
+# Валидация наличия файла
+if [[ ! -f "$VIDEO_FILE" ]]; then
+  echo "[ОШИБКА] Файл 123.webm не найден в $VIDEO_DIR"
+  read -r
+  exit 1
+fi
+
+# Валидация расширение видео 
+if [[ "${VIDEO_FILE##*.}" != "webm" ]]; then
+  echo "[ПРЕДУПРЕЖДЕНИЕ] Файл не является .webm"
+  read -r
+  exit 1
+fi
+
+echo "Файл найден: $VIDEO_FILE"
+echo
+echo "Запуск сборки VPK..."
+echo
+
+# Сборка vpk
+if ! $VPK_CMD "$VPK_DIR"; then
+  echo
+  echo "[ОШИБКА] Сборка VPK завершилась с ошибкой."
+  read -r
+  exit 1
+fi
+
+echo
+echo "[ГОТОВО] ${VPK_DIR}.vpk успешно создан."
+echo
+echo "Нажмите Enter для выхода..."
+read -r
--- vkp-create-tool/create-vpk-windows.bat
+++ vkp-create-tool/create-vpk-windows.bat
@@ -1,1 +1,60 @@
-vpk.exe pak01_dir
+@echo off
+chcp 65001 >nul
+setlocal EnableExtensions EnableDelayedExpansion
+
+echo ==============================
+echo  СБОРКА VPK ИЗ pak03_dir
+echo ==============================
+echo.
+
+REM Валидация vpk.exe
+if not exist "vpk.exe" (
+    echo [ОШИБКА] vpk.exe не найден в текущей папке.
+    echo Запускайте скрипт рядом с vpk.exe
+    echo.
+    pause
+    exit /b 1
+)
+
+REM Валидация папки
+if not exist "pak03_dir\123" (
+    echo [ОШИБКА] Папка pak03_dir\123 не найдена.
+    echo.
+    pause
+    exit /b 1
+)
+
+REM Валидация файла
+if not exist "pak03_dir\123\123.webm" (
+    echo [ОШИБКА] Файл 123.webm не найден.
+    echo.
+    pause
+    exit /b 1
+)
+
+REM Валидация расширения
+set "file=pak03_dir\123\123.webm"
+if /I not "%file:~-5%"==".webm" (
+    echo [ОШИБКА] Файл не является .webm
+    echo.
+    pause
+    exit /b 1
+)
+
+echo Файл найден: pak03_dir\123\123.webm
+echo.
+echo Запуск сборки VPK...
+echo.
+
+vpk.exe pak03_dir
+
+if errorlevel 1 (
+    echo.
+    echo [ОШИБКА] Сборка VPK завершилась с ошибкой.
+) else (
+    echo.
+    echo [ГОТОВО] pak03_dir.vpk успешно создан.
+)
+
+echo.
+pause
